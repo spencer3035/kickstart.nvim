1bbe929d488475fd33bb1ffb938d669d7568ab13
Spencer <spencer3035@gmail.com>
2025-01-17 18:39:40 +0000

    Updated comments and packages

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,7 +1,5 @@
 local jdtls_path = require('mason-registry').get_package('jdtls'):get_install_path()
-local jdtls_bin = jdtls_path .. '/jdtls'
 local jdtls_config = jdtls_path .. '/../../share/jdtls'
-local lombok_jar = '/home/spencer.littel_cn/.m2/repository/org/projectlombok/lombok/1.18.34/lombok-1.18.34.jar'
 
 local home = vim.fn.expand '~'
 local mason_path = vim.fn.stdpath 'data' .. '/mason/packages/jdtls'
@@ -30,57 +28,12 @@ local config = {
     '--add-opens',
     'java.base/java.lang=ALL-UNNAMED',
     '-jar',
-    --vim.fn.glob(mason_path .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
     jdtls_config .. '/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar',
     '-configuration',
     mason_path .. '/config_linux',
     '-data',
     workspace_path .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t'),
   },
-  --cmd = {
-  --  jdtls_path,
-  --  '-javaagent',
-  --  '/home/spencer.littel_cn/.m2/repository/org/projectlombok/lombok/1.18.34/lombok-1.18.34.jar',
-  --},
-
-  --cmd = {
-  --jdtls_bin,
-  --'--jvm-arg=-javaagent /home/spencer.littel_cn/.m2/repository/org/projectlombok/lombok/1.18.34/lombok-1.18.34.jar',
-
-  -- 💀
-  --'java', -- or '/path/to/java17_or_newer/bin/java'
-  ---- depends on if `java` is in your $PATH env variable and if it points to the right version.
-
-  --'-Declipse.application=org.eclipse.jdt.ls.core.id1',
-  --'-Dosgi.bundles.defaultStartLevel=4',
-  --'-Declipse.product=org.eclipse.jdt.ls.core.product',
-  --'-Dlog.protocol=true',
-  --'-Dlog.level=ALL',
-  --'-Xmx1g',
-  --'--add-modules=ALL-SYSTEM',
-  --'--add-opens',
-  --'java.base/java.util=ALL-UNNAMED',
-  --'--add-opens',
-  --'java.base/java.lang=ALL-UNNAMED',
-
-  ---- 💀
-  --'-jar',
-  --jdtls_config .. '/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar',
-  ---- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
-  ---- Must point to the                                                     Change this to
-  ---- eclipse.jdt.ls installation                                           the actual version
-
-  ---- 💀
-  --'-configuration',
-  --jdtls_config,
-  --'/config_linux',
-  -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
-  -- Must point to the                      Change to one of `linux`, `win` or `mac`
-  -- eclipse.jdt.ls installation            Depending on your system.
-
-  -- See `data directory configuratikon` section in the README
-  --'-data', '/path/to/unique/per/project/workspace/folder'
-  --},
   root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
 }
 
