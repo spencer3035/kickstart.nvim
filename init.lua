a4b1266947c7a85b160fd04ccc5c74aaf07728cb
Spencer <spencer3035@gmail.com>
2025-04-11 10:02:57 -0600

    Removed confirm close prompt

--- init.lua
+++ init.lua
@@ -159,7 +159,7 @@ vim.opt.scrolloff = 10
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
-vim.opt.confirm = true
+vim.opt.confirm = false
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
