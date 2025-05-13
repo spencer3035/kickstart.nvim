4f790ddf96f64c8bee474269a3d31043147360d3
Spencer <spencer3035@gmail.com>
2025-05-13 16:36:26 -0600

    Switched accept key to Ctrl-Enter instead of Ctrl-Space

--- init.lua
+++ init.lua
@@ -866,7 +866,7 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
-        ['<C-space>'] = { 'select_and_accept', 'fallback' },
+        ['<C-enter>'] = { 'select_and_accept', 'fallback' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
